f2e482701c40f34e765396741b9b169cc0303598
Leon Stafford <me@ljs.dev>
2021-09-04 14:10:23 +1000

    +category template

---
+++ category.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The category template file
+ *
+ * @package AccessibleMinimalism
+ */
+
+get_header();
+
+$category = get_queried_object();
+
+echo get_cat_name( $category->term_id );
+
+if ( have_posts() ) {
+?>
+
+<ul>
+
+<?php
+
+    // Load posts loop.
+    while ( have_posts() ) {
+        the_post();
+
+?>
+       <li> 
+        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+            <?php the_title('', ''); ?></a>
+
+        <?php echo get_the_date(); ?>
+
+        </li>
+
+<?php 
+    } // end posts loop
+?>
+
+</ul>
+
+<?php
+} else {
+
+        echo "<h2>No content found</h2>";
+ 
+        echo "<p>Put something here</p>";
+        
+
+}
+
+get_footer();
